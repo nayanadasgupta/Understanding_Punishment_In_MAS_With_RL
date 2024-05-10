539b936a566a4903a31d4760dbbd9555fa41f052
nayanadasgupta <nayana.dasgupta@outlook.com>
2024-05-10 22:21:53 +0100

    Change to 64 nodes in hidden layer

--- DQN/Network.py
+++ DQN/Network.py
@@ -6,8 +6,8 @@ import torch.nn.functional as F
 class Network(nn.Module):
     def __init__(self, input_dim: int, output_dim: int):
         super(Network, self).__init__()
-        self.fc_1 = nn.Linear(input_dim, 128)
-        self.fc_3 = nn.Linear(128, output_dim)
+        self.fc_1 = nn.Linear(input_dim, 64)
+        self.fc_3 = nn.Linear(64, output_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = F.relu(self.fc_1(x))
